14081539b0328bf3ef8dc7118ca20731d2b7ef15
Chihau Chau <chihau@gmail.com>
2018-03-22 02:14:45 -0300

    First commit

---
+++ app/src/main/java/cl/chihau/mytestapplication/Utils.java
@@ -0,0 +1,13 @@
+package cl.chihau.mytestapplication;
+
+/**
+ * Created by chihau on 22-03-18.
+ */
+
+public class Utils {
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
